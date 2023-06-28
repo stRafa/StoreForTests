7f5ef8829f2ebefedd08485e0b14c12a46f0685e
stRafa <rafael.familiamarques2016@gmail.com>
2023-06-28 15:19:14 -0300

    Added Validations

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using Flunt.Validations;
 
 namespace Store.Domain.Entities
 {
@@ -10,14 +8,21 @@ namespace Store.Domain.Entities
     {
         public OrderItem(Product product, int quantity)
         {
+            AddNotifications(
+                new Contract<OrderItem>()
+                    .Requires()
+                    .IsNotNull(product, "Product", "Produto inválido")
+                    .IsGreaterThan(quantity, 0, "Quantity", "A quantidade deve ser maior que zero")
+            );
+
             Product = product;
             Price = Product != null ? product.Price : 0;
             Quantity = quantity;
         }
 
-        public Product? Product { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        public Product? Product { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
 
         public decimal Total()
         {
