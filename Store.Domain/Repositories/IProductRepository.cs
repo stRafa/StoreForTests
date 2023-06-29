7e76f9ef30636d6e373e2a7fe4756eb5cbb94f0c
stRafa <rafael.familiamarques2016@gmail.com>
2023-06-29 11:11:21 -0300

    Added Queries and tested

---
+++ Store.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories.Interfaces
+{//existem para atender necessidades e não pra simplesmente implementar CRUD
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Get(IEnumerable<Guid> ids);
+    }
+}
