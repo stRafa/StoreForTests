1c787c185a648e7acc7c01df89d89e82733a1a32
stRafa <rafael.familiamarques2016@gmail.com>
2023-06-29 12:05:50 -0300

    Added Handlers and Tested

---
+++ Store.Domain/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,10 @@
+﻿using Store.Domain.Commands.Interfaces;
+using Store.Domain.Repositories.Interfaces;
+
+namespace Store.Domain.Handlers.Interfaces
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
